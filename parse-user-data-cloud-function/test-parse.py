c4bc9b6abca4a093d2076c909ab95cee672e13c0
fakhrip <fakhriputra123s@gmail.com>
2021-05-30 17:33:38 +0700

    fix jwt parsing error

--- parse-user-data-cloud-function/test-parse.py
+++ parse-user-data-cloud-function/test-parse.py
@@ -1,9 +1,11 @@
 import jwt
 
-print(
-    jwt.decode(
+try:
+    res = jwt.decode(
         "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJodHRwczovL2hhc3VyYS5pby9qd3QvY2xhaW1zIjp7IngtaGFzdXJhLWFsbG93ZWQtcm9sZXMiOlsidXNlciJdLCJ4LWhhc3VyYS1kZWZhdWx0LXJvbGUiOiJ1c2VyIiwieC1oYXN1cmEtdXNlci1pZCI6IjIifX0.9LfRSxwLmytBqlau1nbzCnp999Y-a0UvCDs-7QYSQvI",
         "eW91IHJlYWxseSB0aGluayBpbSB0aGF0IHN0dXBpZCA/Cg==",
         algorithms=["HS256"],
     )
-)
+    print(res)
+except:
+    print("Error happened")
