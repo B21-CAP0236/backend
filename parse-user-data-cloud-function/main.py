67234b1306dd0031f45ccf6522e7c696714471a4
fakhrip <fakhriputra123s@gmail.com>
2021-06-03 11:19:40 +0700

    add cors preflight request handler

--- parse-user-data-cloud-function/main.py
+++ parse-user-data-cloud-function/main.py
@@ -4,7 +4,25 @@ from flask import abort
 
 
 def parse(request):
-    if request.method == "POST":
+
+    # Set CORS headers for the main request
+    headers = {
+        'Access-Control-Allow-Origin': '*'
+    }
+
+    if request.method == 'OPTIONS':
+        # Allows GET requests from any origin with the Content-Type
+        # header and caches preflight response for an 3600s
+        headers = {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'GET',
+            'Access-Control-Allow-Headers': 'Content-Type',
+            'Access-Control-Max-Age': '3600'
+        }
+
+        return ('', 204, headers)
+        
+    elif request.method == "POST":
         content_type = request.headers["content-type"]
         if content_type == "application/json":
             request_json = request.get_json(silent=True)
